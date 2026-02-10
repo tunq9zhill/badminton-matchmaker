e0f372b42d40cf57c1977eabbce363e420512f7c
WhaleBaiTong <ovalet@outlook.co.th>
2026-02-10 18:51:13 +0700

    create root main tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { render } from "react-dom";
-import App from "./App.jsx";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./styles/index.css";
 
-const container = document.getElementById("root");
- render(
+createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
- </React.StrictMode>,
-  container
+  </React.StrictMode>
 );
